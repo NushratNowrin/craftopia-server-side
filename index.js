a289f0f3a6c931aa353dbab423a4ab37037ccfcf
NushratNowrin <nowrin2023@gmail.com>
2023-06-16 20:11:42 +0600

    middleware declared

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Craftopia is running')
